aee931c89391edfb0fed1ac73723bdc1a2b77ddc
Md.Afizul Islam <afizul2459@gmail\.com>
2026-02-20 23:17:57 +0600

    Initial commit

--- src/assets/Components/MyWork/MyWork.jsx
+++ src/assets/Components/MyWork/MyWork.jsx
@@ -19,19 +19,19 @@ const MyWork = () => {
                         <h3>Portfolio Website</h3>
                         <p>React + Animation</p>
                         <div className="work-btns">
-                            <a href="https://afizul2459.github.io/agricultural-information/">Live</a>
-                            <a href="#">Github</a>
+                            <a href="https://afizul2459.github.io/afizul-react-portfolio/">Live</a>
+                            <a href="https://github.com/Afizul2459/afizul-react-portfolio">Github</a>
                         </div>
                     </div>
                 </div>
 
                 <div className="work-card">
-                    <img src="https://images.unsplash.com/photo-1556740720-776b84291f6b?w=600" alt="" />
+                    <img src="https://afizul2459.github.io/agricultural-information/" alt="" />
                     <div className="work-overlay">
                         <h3>POS System</h3>
                         <p>Laravel + MySQL</p>
                         <div className="work-btns">
-                            <a href="#">Live</a>
+                            <a href="https://afizul2459.github.io/agricultural-information/">Live</a>
                             <a href="#">Github</a>
                         </div>
                     </div>
